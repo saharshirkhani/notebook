0c47a03fdd34125968ca6989731ac64fa01fd20f
sahar <s.sh200018@yahoo.com>
2020-12-10 20:30:48 +0330

    Navigation menu

--- app/src/main/java/com/google/notebook/MainActivity.java
+++ app/src/main/java/com/google/notebook/MainActivity.java
@@ -1,14 +1,39 @@
 package com.google.notebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.all){
+                    Intent intent=new Intent(MainActivity.this,Note.class);
+                    startActivity(intent);
+                }
+                else if (item.getItemId()==R.id.add){
+                    Intent intent=new Intent(MainActivity.this,Add_note.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
     }
 }
